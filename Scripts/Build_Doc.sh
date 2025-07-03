2634d5ea91796f80b7c88e14b37c123851e8f8f8
laosb <git@lao.sb>
2025-07-03 13:42:31 +0800

    fix: build script

--- Scripts/Build_Doc.sh
+++ Scripts/Build_Doc.sh
@@ -1,6 +1,8 @@
+set -e
+
 sudo xcode-select -s /Applications/Xcode_16.2.0.app
 
-xcodebuild docbuild -scheme EasyRichText-Package -destination 'platform=iOS Simulator,name=iPhone 15' -parallelizeTargets -derivedDataPath docsData
+xcodebuild docbuild -scheme EasyRichText-Package -destination 'platform=macOS,name=Any Mac' -parallelizeTargets -derivedDataPath docsData
 echo "Copying DocC archives to docArchives..."
 mkdir -p docArchives
 cp -R `find docsData -type d -name "*.doccarchive"` docArchives
