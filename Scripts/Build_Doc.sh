b732c80cbcd87feb885b50ad5e32881403dd8a2d
laosb <git@lao.sb>
2025-07-03 13:50:52 +0800

    ci: fix destination specifier

--- Scripts/Build_Doc.sh
+++ Scripts/Build_Doc.sh
@@ -2,7 +2,7 @@ set -e
 
 sudo xcode-select -s /Applications/Xcode_16.2.0.app
 
-xcodebuild docbuild -scheme EasyRichText-Package -destination 'platform=macOS,name=Any Mac' -parallelizeTargets -derivedDataPath docsData
+xcodebuild docbuild -scheme EasyRichText-Package -destination 'platform=macOS,arch=arm64' -parallelizeTargets -derivedDataPath docsData
 echo "Copying DocC archives to docArchives..."
 mkdir -p docArchives
 cp -R `find docsData -type d -name "*.doccarchive"` docArchives
